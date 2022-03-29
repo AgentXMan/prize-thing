68ef8843b328efc0e882f62ef22e1cac234c0bcd
AgentXMan <66519000+AgentXMan@users.noreply.github.com>
2022-03-29 19:59:26 +0400

    Update stock prize.py

--- stock prize.py
+++ stock prize.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-data = pd.read_csv("yahoo_stock (1) (1).csv")
+data = pd.read_csv("yahoo_stock (1) (1).csv") #reads the file
 df = pd.DataFrame(data) #makes a spreadsheet and then stores it
 
 
